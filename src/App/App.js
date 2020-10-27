f4f3f35a52b077818ac0caff325fba9dc3fc65b0
syq <1035394794@qq.com>
2020-10-27 22:05:08 +0800

    [syq] feat: add home page

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.scss';
+import Home from '../pages/Home'
 
 class App extends Component {
   render() {
     return (
       <div data-testid="app" className="App">
-        Hello World
+        <Home />
       </div>
     );
   }
