4f3c99cb9c3101683bd3c1664c09923c072da7fa
kevindongzg <dongkevin94@gmail.com>
2020-11-02 16:57:49 +0800

    add TODOs

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react';
 import './App.scss';
-import Home from '../pages/Home'
+import Home from '../pages/Home';
 
+// TODO GTB-1: * 页面样式还原度较高，且实现都是通过接口方式
+// TODO GTB-1: * 除了刷新页面没有立即显示已分组情况之外，功能全部实现
+// TODO GTB-2: * 没有测试
+// TODO GTB-3: * 没有做组件拆分，只有一个Home组件，思考如何拆分与复用组件
+// TODO GTB-3: * 语义化标签使用不好
+// TODO GTB-3: * 使用了flex和scss嵌套
+// TODO GTB-3: * 运用了ES6+语法及axios
+// TODO GTB-3: * 运用React相关知识点，但未拆分组件，一些知识点无法验证
+// TODO GTB-4: * 有小步提交意识，但提交可以更小步，message更明确，如拆分组件后以组件粒度等
+// TODO GTB-4: * 没有抽出Api请求层
+// TODO GTB-4: * 没有做组件拆分，Home组件过长
+// TODO GTB-4: * 没有组件拆分所以scss也没有拆分
+// TODO GTB-4: * eslint error 较多，需要fix
 class App extends Component {
   render() {
     return (
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,101 +1,119 @@
 import React, { Component } from 'react';
-import './Home.scss'
-import axios from "axios"
+import './Home.scss';
+import axios from 'axios';
 
+// TODO GTB-4: - 没有组件拆分，Home包含内容逻辑较多，组件过长
 class Home extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
       students: [],
       groups: [],
       isAdd: true,
-      name: ''
-    }
+      // TODO GTB-4: - name state比较冗余，这里不用做双向绑定
+      name: '',
+    };
   }
 
   componentDidMount() {
-    this.getAllStudents()
+    this.getAllStudents();
   }
 
   getAllStudents = () => {
+    // TODO GTB-4: - 请求相关可以拆出API请求层，解耦请求与渲染
     axios.get('http://localhost:8080/students').then((res) => {
       this.setState({
-        students: res.data
-      })
-    })
-  }
+        students: res.data,
+      });
+    });
+  };
 
   grouping = () => {
     axios.get('http://localhost:8080/groups').then((res) => {
       this.setState({
-        groups: res.data
-      })
-    })
-  }
+        groups: res.data,
+      });
+    });
+  };
 
   addStudent = () => {
     this.setState({
-      isAdd: false
-    })
-  }
+      isAdd: false,
+    });
+  };
 
+  // TODO GTB-4: - 同name，这里这个方法也是冗余的
   changeText = (e) => {
     this.setState({
-      name: e.target.value
-    })
-  }
+      name: e.target.value,
+    });
+  };
 
+  // TODO GTB-3: - 下面的e中可以取到value即为name
   enterAdd = (e) => {
     if (e.keyCode === 13) {
       this.setState({
-        isAdd: true
-      })
+        isAdd: true,
+      });
       axios.post('http://localhost:8080/student', { name: this.state.name }).then(() => {
-        this.getAllStudents()
-      })
+        this.getAllStudents();
+      });
     }
-  }
+  };
 
   render() {
+    // TODO GTB-4: - 加强语义化标签的使用
+    // TODO GTB-3: - 没有做组件拆分，嵌套比较深，易读性低
+    // TODO GTB-4: - 思考如何拆分组件，如学员列表，分组列表，和组件复用，如学员item等
     return (
       <div className="main">
+        {/* TODO GTB-4: - 标签class命名需要更加体现业务逻辑，top之类的无法体现，且可能冲突 */}
         <div className="top">
           <span className="text">分组列表</span>
-          <button className="group-btn" onClick={this.grouping}>分组学员</button>
+          <button className="group-btn" onClick={this.grouping}>
+            分组学员
+          </button>
         </div>
-        {
-          this.state.groups.length > 0 ?
-            <div className="groups">
-            {
-              this.state.groups && this.state.groups.map((group, index) => (
+        {this.state.groups.length > 0 ? (
+          <div className="groups">
+            {this.state.groups &&
+              this.state.groups.map((group, index) => (
+                // TODO GTB-4: - 不推荐使用index作为key
                 <div className="group" key={index}>
                   <div className="group-num">{`${index + 1}组`}</div>
                   <div className="group-list">
-                  {
-                    group.map((student, index) => (<div className="item" key={index}>{`${student.id}.${student.name}`}</div>))
-                  }
+                    {group.map((student, index) => (
+                      <div className="item" key={index}>{`${student.id}.${student.name}`}</div>
+                    ))}
                   </div>
                 </div>
-              ))
-            }
-            </div>
-            : ''
-        }
-        
+              ))}
+          </div>
+        ) : (
+          ''
+        )}
+
         <div className="student-list">
           <span className="text">学员列表</span>
           <div className="list">
-            {
-              this.state.students && this.state.students.map((student, index) => (<div className="item" key={index}>{`${student.id}.${student.name}`}</div>))
-            }
-            {
-              this.state.isAdd ?
-                <div className="item" onClick={this.addStudent}>+添加学员</div>
-                :
-                <div className="item">
-                  <input type="text" value={this.state.name} onChange={this.changeText} onKeyUp={this.enterAdd} />
-                </div>
-            }
+            {this.state.students &&
+              this.state.students.map((student, index) => (
+                <div className="item" key={index}>{`${student.id}.${student.name}`}</div>
+              ))}
+            {this.state.isAdd ? (
+              <div className="item" onClick={this.addStudent}>
+                +添加学员
+              </div>
+            ) : (
+              <div className="item">
+                <input
+                  type="text"
+                  value={this.state.name}
+                  onChange={this.changeText}
+                  onKeyUp={this.enterAdd}
+                />
+              </div>
+            )}
           </div>
         </div>
       </div>
